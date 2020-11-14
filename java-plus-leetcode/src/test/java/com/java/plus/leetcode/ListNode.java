7edd1d8d97729cb69a09cc530cc484e64c97bee7
jianjunyue <jianjunyue@gmail.com>
2020-11-14 20:58:52 +0800

    java leetcode

---
+++ java-plus-leetcode/src/test/java/com/java/plus/leetcode/ListNode.java
@@ -0,0 +1,11 @@
+package com.java.plus.leetcode;
+
+public class ListNode {
+	int val;
+	ListNode next;
+	
+	public ListNode(int value) {
+		val=value;
+	}
+
+}
--- java-plus-leetcode/src/test/java/com/java/plus/leetcode/Test.java
+++ java-plus-leetcode/src/test/java/com/java/plus/leetcode/Test.java
@@ -1,10 +1,41 @@
 package com.java.plus.leetcode;
 
+import com.java.plus.leetcode.model.ListNode; 
+
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		ListNode node = createListNode();
+		print(node);
+		print(reverse(node));
+	}
+	
+	public static ListNode createListNode() {
+		ListNode head = new ListNode(1);//创建头节点
+		head.next = new ListNode(2);//再定义头节点的next域
+		ListNode t = head.next;
+		for(int i=3;i<10;i++) {//创建一个简单的链表{1,2,3,4,5,...,9}
+			t.next = new ListNode(i);
+			t = t.next;
+		}
+		return head;
+	}
+	
+	public static void print(ListNode node) {
+		while(node!=null) {
+			System.out.print(node.val+"->");
+			node=node.next;
+		}
+		System.out.println();
+	}
+	
+	public static ListNode reverse(ListNode node) {
+		if(node.next==null)
+			return node;
+		ListNode last=reverse(node.next);
+		node.next.next=node;
+		node.next=null;
+		return last; 
 	}
 
 }
