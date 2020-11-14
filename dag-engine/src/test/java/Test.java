bc7768620c8361f47ee4d42a88efa45a56b9c917
jianjunyue <jianjunyue@gmail.com>
2020-11-14 14:23:44 +0800

    java dag

--- dag-engine/src/test/java/Test.java
+++ dag-engine/src/test/java/Test.java
@@ -1,21 +1,33 @@
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.atomic.AtomicInteger;
-
-public class Test {
-
-	public static void main(String[] args) {
-		 Map<String, AtomicInteger> inDegreeAtomicIntMap=new HashMap<String, AtomicInteger>();
-		 inDegreeAtomicIntMap.put("1", new AtomicInteger(1));
-		 inDegreeAtomicIntMap.put("2", new AtomicInteger(200));
-		 inDegreeAtomicIntMap.put("3", new AtomicInteger(300));
-		 System.out.println(inDegreeAtomicIntMap);
-		 
-		 inDegreeAtomicIntMap.get("1").decrementAndGet();
-		 inDegreeAtomicIntMap.get("1").decrementAndGet();
-		 inDegreeAtomicIntMap.get("2").decrementAndGet(); 
-
-			 System.out.println(inDegreeAtomicIntMap);
-	}
-
-}
+//import java.util.HashMap;
+//import java.util.Map;
+//import java.util.concurrent.ArrayBlockingQueue;
+//import java.util.concurrent.ExecutorService;
+//import java.util.concurrent.SynchronousQueue;
+//import java.util.concurrent.ThreadPoolExecutor;
+//import java.util.concurrent.TimeUnit;
+//import java.util.concurrent.atomic.AtomicInteger;
+//
+//public class Test {
+//
+//	public static void main(String[] args) {
+//		 Map<String, AtomicInteger> inDegreeAtomicIntMap=new HashMap<String, AtomicInteger>();
+//		 inDegreeAtomicIntMap.put("1", new AtomicInteger(1));
+//		 inDegreeAtomicIntMap.put("2", new AtomicInteger(200));
+//		 inDegreeAtomicIntMap.put("3", new AtomicInteger(300));
+//		 System.out.println(inDegreeAtomicIntMap);
+//		 
+//		 inDegreeAtomicIntMap.get("1").decrementAndGet();
+//		 inDegreeAtomicIntMap.get("1").decrementAndGet();
+//		 inDegreeAtomicIntMap.get("2").decrementAndGet(); 
+//
+//			 System.out.println(inDegreeAtomicIntMap);
+//	}
+//	
+//    private final static ExecutorService executorPool = new ThreadPoolExecutor(50, 500, 5, TimeUnit.SECONDS, new ArrayBlockingQueue<>(1000));
+//    
+//	
+//    public final static ThreadPoolExecutor EXECUTOR_POOL = new ThreadPoolExecutor(20, Integer.parseInt("600"),
+//            60, TimeUnit.SECONDS, new SynchronousQueue<>(), new DefaultThreadFactory("coreThreadPool-"),
+//            new ThreadPoolExecutor.CallerRunsPolicy());
+//
+//}
--- java-plus-dag/src/test/java/org/java/plus/dag/Test.java
+++ java-plus-dag/src/test/java/org/java/plus/dag/Test.java
@@ -1,13 +1,26 @@
 package org.java.plus.dag;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.java.plus.dag.core.base.model.ProcessorContext;
 
 public class Test {
 
 	public static void main(String[] args) {
-		RecommendSolution rec=new RecommendSolution();
-		ProcessorContext context=new ProcessorContext();
-		rec.doRecommend(context);
+//		RecommendSolution rec=new RecommendSolution();
+//		ProcessorContext context=new ProcessorContext();
+//		rec.doRecommend(context);
+		
+		List<String> list=new ArrayList<String>();
+		list.add("1");
+		list.add("2");
+		list.add("3");
+		
+		Object[] obj=list.stream().filter(x-> "2".equals(x)).toArray();
+
+		System.out.println(obj.length);
+		System.out.println(obj[0]);
 	}
 
 }
